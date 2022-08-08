44190ff4b8629d360309b21e6275eb44731194b1
Ahmed Ayman <email1.im4a@gmail.com>
2022-08-08 15:39:41 +0200

    :recycle: Moved token extractor from browser to its own class

---
+++ LLCD.CourseExtractor/TokenExtractor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LLCD.CourseExtractor
+{
+    public static class TokenExtractor
+    {
+        public static string ExtractToken(Browser browser)
+        {
+            var cookieExtractor = new CookiesExtractor(".www.linkedin.com");
+            List<DBCookie> cookies;
+            switch (browser)
+            {
+                case Browser.Chrome:
+                    cookies = cookieExtractor.ReadChromeCookies();
+                    break;
+                case Browser.Firefox:
+                    cookies = cookieExtractor.ReadFirefoxCookies();
+                    break;
+                case Browser.Edge:
+                    cookies = cookieExtractor.ReadEdgeCookies();
+                    break;
+                default:
+                    throw new ArgumentException("browser");
+            }
+            if (cookies.Where(c => c.Name == "li_at").Count() != 0)
+            {
+                return cookies.Where(c => c.Name == "li_at").First().Value;
+            }
+            return null;
+        }
+    }
+}
