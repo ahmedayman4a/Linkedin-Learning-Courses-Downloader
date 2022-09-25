1b2a0fed0b21d8211ba2d5197192fe8eb74f9ebe
Ahmed Ayman <email1.im4a@gmail.com>
2022-09-25 19:53:41 +0200

    :sparkles: Add learning path extraction

---
+++ LLCD.CourseContent/LearningPath.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LLCD.CourseContent
+{
+    [JsonConverter(typeof(JsonPathConverter))]
+    public class LearningPath
+    {
+        [JsonProperty("elements[0].sections[*].items[*].content.*.slug")]
+        public List<string> CoursesSlugs { get; set; }
+
+        public static LearningPath FromJson(string json) => JsonConvert.DeserializeObject<LearningPath>(json, Converter.Settings);
+
+        public override bool Equals(object obj)
+        {
+            return obj is LearningPath learningPath &&
+                   CoursesSlugs.SequenceEqual(learningPath.CoursesSlugs);
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1331293932;
+            hashCode = hashCode * -1521134295 + EqualityComparer<List<string>>.Default.GetHashCode(CoursesSlugs);
+            return hashCode;
+        }
+    }
+}
---
+++ LLCD.CourseExtractor/LearningPathExtractor.cs
@@ -0,0 +1,30 @@
+﻿using LLCD.CourseContent;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LLCD.CourseExtractor
+{
+    public class LearningPathExtractor
+    {
+        private readonly string _slug;
+        private readonly ExtractionSession _session;
+
+        public LearningPathExtractor(string slug, string token)
+        {
+            _slug = slug;
+            _session = new ExtractionSession(token);
+        }
+
+        public async Task<LearningPath> GetLearningPath()
+        {
+            var response = await _session.GetResponse($"https://linkedin.com/learning-api/detailedLearningPaths?learningPathSlug={_slug}&q=slug&version=2");
+            var learningPathJson = await response.Content.ReadAsStringAsync();
+
+            LearningPath learningPath = LearningPath.FromJson(learningPathJson);
+
+            return learningPath;
+        }
+    }
+}
