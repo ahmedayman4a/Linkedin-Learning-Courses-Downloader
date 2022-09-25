12cfc27f0b96f5a11ee28dec73719b40b34f7342
Ahmed Ayman <email1.im4a@gmail.com>
2022-09-25 20:05:52 +0200

    :sparkles: Add BuildExtractors that takes urls and returns a list of courses extractors

---
+++ LLCD.CourseExtractor/CoursesExtractorsBuilder.cs
@@ -0,0 +1,45 @@
+﻿using LLCD.CourseContent;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace LLCD.CourseExtractor
+{
+    public static class CoursesExtractorsBuilder
+    {
+
+        public static async Task<List<Extractor>> BuildExtractors(List<string> urls, Quality quality, string token, int delay = 0)
+        {
+            var extractors = new List<Extractor>();
+
+            foreach (var url in urls)
+            {
+                var urlType = UrlTypeHelper.CategorizeUrl(url,out string slug);
+                switch (urlType)
+                {
+                    case UrlType.Course:
+                        var extractor = new Extractor(slug, quality,token,delay);
+                        extractors.Add(extractor);
+                        break;
+                    case UrlType.LearningPath:
+                        var learningPathExtractor = new LearningPathExtractor(slug, token);
+                        var learningPath = await learningPathExtractor.GetLearningPath();
+                        foreach (string courseSlug in learningPath.CoursesSlugs)
+                        {
+                            var courseExtractor = new Extractor(courseSlug, quality,token,delay);
+                            extractors.Add(courseExtractor);
+                        }
+                        break;
+                    case UrlType.Invalid:
+                    default:
+                        continue;
+                }
+            }
+            return extractors;
+        }
+
+    }
+}
