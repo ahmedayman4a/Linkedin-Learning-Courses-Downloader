6b84ee5879fcbf52e9660276701c35b1c75a7cb0
Ahmed Ayman <email1.im4a@gmail.com>
2022-09-25 20:04:28 +0200

    :art:  Apply changes made by moving some code in seperate classes like validation and sessions

--- LLCD.CourseExtractor/Extractor.cs
+++ LLCD.CourseExtractor/Extractor.cs
@@ -16,74 +16,30 @@ using Serilog;
 
 namespace LLCD.CourseExtractor
 {
-    public class Extractor
+    public class Extractor : IDisposable
     {
-        public delegate void LinksExtractionEventHandler();
         private readonly Quality _quality;
         private readonly int _delay;
-        private string _courseUrl;
-        private string _courseSlug;
-        private HttpClient _client;
-        private CookieContainer _cookieContainer;
-        private string _linkedinHomeRaw;
-        private bool _isTokenChecked = false;
+        private readonly string _courseSlug;
+        private readonly ExtractionSession _extractionSession;
+        
+        
         private static Random _random = new Random();
 
-        public string EnterpriseProfileHash { get; set; }
 
-        public Extractor(string courseUrl, Quality quality, string token, int delay = 0)
+        public Extractor(string courseSlug, Quality quality, string token, int delay = 0)
         {
-            _courseUrl = courseUrl;
+            _courseSlug = courseSlug;
             _quality = quality;
             _delay = delay;
-            _cookieContainer = new CookieContainer();
-            _cookieContainer.Add(new Cookie("li_at", token, "/", ".www.linkedin.com"));
-            var clienthandler = new HttpClientHandler { UseCookies = true, CookieContainer = _cookieContainer };
-            _client = new HttpClient(clienthandler);
-            _client.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0");
+            _extractionSession = new ExtractionSession(token);
         }
 
-        public static string ExtractToken(Browser browser)
-        {
-            var cookieExtractor = new CookiesExtractor(".www.linkedin.com");
-            List<DBCookie> cookies;
-            switch (browser)
-            {
-                case Browser.Chrome:
-                    cookies = cookieExtractor.ReadChromeCookies();
-                    break;
-                case Browser.Firefox:
-                    cookies = cookieExtractor.ReadFirefoxCookies();
-                    break;
-                case Browser.Edge:
-                    cookies = cookieExtractor.ReadEdgeCookies();
-                    break;
-                default:
-                    throw new ArgumentException("browser");
-            }
-            if (cookies.Where(c => c.Name == "li_at").Count() != 0)
-            {
-                return cookies.Where(c => c.Name == "li_at").First().Value;
-            }
-            return null;
-        }
+        
 
         public async Task<Course> GetCourse(IProgress<float> progress = null)
         {
-            if (!HasValidUrl())
-            {
-                throw new ArgumentException("Invalid Course Url : " + _courseUrl);
-            }
-            if (!_isTokenChecked && !await HasValidToken())
-            {
-                throw new ArgumentException("Invalid Token");
-            }
-            EnterpriseProfileHash = await ExtractEnterpriseProfileHash();
-            if (!String.IsNullOrEmpty(EnterpriseProfileHash))
-            {
-                _client.DefaultRequestHeaders.Add("x-li-identity", EnterpriseProfileHash);
-            }
-            var courseResponse = await _client.GetAsync($"https://www.linkedin.com/learning-api/detailedCourses?courseSlug={_courseSlug}&fields=chapters,title,exerciseFiles&addParagraphsToTranscript=true&q=slugs");
+            var courseResponse = await _extractionSession.GetResponse($"https://www.linkedin.com/learning-api/detailedCourses?courseSlug={_courseSlug}&fields=chapters,title,exerciseFiles&addParagraphsToTranscript=true&q=slugs");
             var courseResponseText = await courseResponse.Content.ReadAsStringAsync();
 
             Course course;
@@ -116,7 +72,7 @@ namespace LLCD.CourseExtractor
                     {
                         var video = chapter.Videos[i];
                         string slug = video.Slug;
-                        var videoResponse = await _client.GetAsync($"https://www.linkedin.com/learning-api/detailedCourses?courseSlug={_courseSlug}&resolution=_{_quality.ToHeight()}&q=slugs&fields=selectedVideo&videoSlug={video.Slug}");
+                        var videoResponse = await _extractionSession.GetResponse($"https://www.linkedin.com/learning-api/detailedCourses?courseSlug={_courseSlug}&resolution=_{_quality.ToHeight()}&q=slugs&fields=selectedVideo&videoSlug={video.Slug}");
                         var videoResponseText = await videoResponse.Content.ReadAsStringAsync();
 
                         try
@@ -131,90 +87,30 @@ namespace LLCD.CourseExtractor
                         video.Slug = slug;
                         if (String.IsNullOrWhiteSpace(video.DownloadUrl))
                         {
-                            var cookies = _cookieContainer.GetCookies(new Uri("https://www.linkedin.com/learning-api"));
-                            Log.Error($"Failed to extract a course video.Request Uri : {videoResponse.RequestMessage.RequestUri}\nResponseMessage : {videoResponse.StatusCode} - {videoResponse.ReasonPhrase}\nResponseText : {videoResponseText}");
+                            Log.Error($"Failed to extract a course video. Request Uri : {videoResponse.RequestMessage.RequestUri}\nResponseMessage : {videoResponse.StatusCode} - {videoResponse.ReasonPhrase}\nResponseText : {videoResponseText}");
                             throw new ArgumentException("Failed to extract a course video. The provided token is probably invalid");
                         }
                         chapter.Videos[i] = video;
                         progress?.Report(j / totalCount);
-                        double randomDelay = _delay + _random.NextDouble();
-                        randomDelay -=  _random.NextDouble();
-                        var delay = TimeSpan.FromMilliseconds(randomDelay * 1000);
-                        await Task.Delay(delay);
+                        if (_delay > 0)
+                        {
+                            double randomDelay = _delay + _random.NextDouble();
+                            randomDelay -= _random.NextDouble();
+                            var delay = TimeSpan.FromMilliseconds(randomDelay * 1000);
+                            await Task.Delay(delay);
+                        }
+                        
                     },
-                    exceptionMessage: "Error occured while getting video inside chapter for loop",
+                    exceptionMessage: "Error occured while getting video inside chapter loop",
                     retries: 3);
             }
             }
             return course;
         }
 
-        public async Task<bool> HasValidToken()
+        public void Dispose()
         {
-            Regex patternTrialLink = new Regex(@"nav__button-tertiary.*\n?.\r?.*Start free trial");
-            if (_linkedinHomeRaw is null)
-            {
-                var response = await _client.GetAsync("https://www.linkedin.com/learning");
-                _linkedinHomeRaw = await response.Content.ReadAsStringAsync();
-                if (!response.IsSuccessStatusCode)
-                {
-                    Log.Error("Home page GET request content : \n" + response.Content);
-                    throw new Exception("Failed to get home page. Error " + response.StatusCode);
-                }
-                _linkedinHomeRaw = WebUtility.HtmlDecode(_linkedinHomeRaw);
-            }
-
-            if (patternTrialLink.IsMatch(_linkedinHomeRaw))
-            {
-                return false;
-            }
-            var cookies = _cookieContainer.GetCookies(new Uri("https://www.linkedin.com/learning"));
-            if (cookies is null)
-            {
-                throw new Exception("No cookies are found for home page.");
-            }
-            var jsession = cookies["JSESSIONID"].Value;
-            if (jsession is null)
-            {
-                throw new Exception("JSESSIONID cookie can't be null.");
-            }
-            _client.DefaultRequestHeaders.Add("Csrf-Token", jsession);
-            _isTokenChecked = true;
-            return true;
-
+            _extractionSession?.Dispose();
         }
-
-        private async Task<String> ExtractEnterpriseProfileHash()
-        {
-            Regex patternEnterpriseProfileHash = new Regex(@"enterpriseProfileHash"":""(?<enterpriseProfileHash>.*?)""");
-            if (_linkedinHomeRaw is null)
-            {
-                var response = await _client.GetAsync("https://www.linkedin.com/learning");
-                _linkedinHomeRaw = await response.Content.ReadAsStringAsync();
-                _linkedinHomeRaw = WebUtility.HtmlDecode(_linkedinHomeRaw);
-            }
-
-            if (patternEnterpriseProfileHash.IsMatch(_linkedinHomeRaw))
-            {
-                return patternEnterpriseProfileHash.Match(_linkedinHomeRaw).Groups["enterpriseProfileHash"].Value;
-            }
-            return null;
-        }
-        public bool HasValidUrl()
-        {
-            if (!_courseUrl.Contains("https://") || !_courseUrl.Contains("http://"))
-            {
-                _courseUrl = "https://" + _courseUrl;
-            }
-            Regex patternCourseUrl = new Regex(@"https?:\/\/(?:www\.)?linkedin\.com\/learning\/(?<courseSlug>[a-zA-Z0-9-]+)");
-
-            if (patternCourseUrl.IsMatch(_courseUrl))
-            {
-                _courseSlug = patternCourseUrl.Match(_courseUrl).Groups["courseSlug"].Value;
-                return true;
-            }
-            return false;
-        }
-
     }
 }
